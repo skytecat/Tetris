a5fff6c126b002abfa7934b2c30499027b00955c
Alyona <107105936+skytecat@users.noreply.github.com>
2024-09-02 09:51:39 +0300

    add tests.h

---
+++ tests/tests.h
@@ -0,0 +1,12 @@
+#ifndef _TESTS_H
+#define _TESTS_H
+#include <check.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+
+#include "../brick_game/tetris/tetris.h"
+#include "../gui/cli/front.h"
+
+Suite *s21_game_suite(void);
+
+#endif
